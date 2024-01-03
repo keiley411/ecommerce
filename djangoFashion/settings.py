f15af053e4117e6182b67eb5dc22162bb47524df
keiley411 <nellymumbi436@gmail.com>
2024-01-03 21:45:02 +0300

    web

--- djangoFashion/settings.py
+++ djangoFashion/settings.py
@@ -15,6 +15,7 @@ import django_heroku
 import dj_database_url
 import whitenoise.storage
 from decouple import config
+from django.core.wsgi import get_wsgi_application
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,7 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-   ' whitenoise.middleware.WhiteNoise'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'djangoFashion.urls'
